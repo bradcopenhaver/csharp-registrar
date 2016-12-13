1985fbf5e7a334e3b817199967674ea48cd2015f
Brad Copenhaver and Steve Burton <brad+steve@>
2016-12-13 15:59:30 -0800

    add individual student view and routes to add course, edit name, and delete

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -45,6 +45,23 @@ namespace Registrar
         model.Add("courseStudents", courseStudents);
         return View["course.cshtml", model];
       };
+      Patch["/course/update/{id}"] = parameters => {
+        Course currentCourse = Course.Find(parameters.id);
+        currentCourse.UpdateName(Request.Form["newName"]);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Student> allStudents = Student.GetAll();
+        List<Student> courseStudents = currentCourse.GetAllStudents();
+        model.Add("course", currentCourse);
+        model.Add("allStudents", allStudents);
+        model.Add("courseStudents", courseStudents);
+        return View["course.cshtml", model];
+      };
+      Delete["/course/delete/{id}"] = parameters => {
+        Course currentCourse = Course.Find(parameters.id);
+        currentCourse.Delete();
+        List<Course> allCourses = Course.GetAll();
+        return View["courses.cshtml", allCourses];
+      };
       Post["/course/add_student"] = _ => {
         Course currentCourse = Course.Find(Request.Form["courseId"]);
         currentCourse.AddStudent(Request.Form["studentId"]);
@@ -56,6 +73,48 @@ namespace Registrar
         model.Add("allStudents", allStudents);
         model.Add("courseStudents", courseStudents);
         return View["course.cshtml", model];
+      };
+      Get["/student/{id}"] = parameters => {
+        Student currentStudent = Student.Find(parameters.id);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Course> allCourses = Course.GetAll();
+        List<Course> studentCourses = currentStudent.GetAllCourses();
+        model.Add("student", currentStudent);
+        model.Add("allCourses", allCourses);
+        model.Add("studentCourses", studentCourses);
+        return View["student.cshtml", model];
+      };
+      Post["/student/add_course"] = _ => {
+        Student currentStudent = Student.Find(Request.Form["studentId"]);
+        currentStudent.AddCourse(Request.Form["courseId"]);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Course> allCourses = Course.GetAll();
+        List<Course> studentCourses = currentStudent.GetAllCourses();
+        model.Add("student", currentStudent);
+        model.Add("allCourses", allCourses);
+        model.Add("studentCourses", studentCourses);
+        return View["student.cshtml", model];
+      };
+      Delete["/student/delete/{id}"] = parameters => {
+        Student currentStudent = Student.Find(parameters.id);
+        currentStudent.Delete();
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+      Get["/students"] = _ => {
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+      Patch["/student/update/{id}"] = parameters => {
+        Student currentStudent = Student.Find(parameters.id);
+        currentStudent.UpdateName(Request.Form["newName"]);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        List<Course> allCourses = Course.GetAll();
+        List<Course> studentCourses = currentStudent.GetAllCourses();
+        model.Add("student", currentStudent);
+        model.Add("allCourses", allCourses);
+        model.Add("studentCourses", studentCourses);
+        return View["student.cshtml", model];
       };
     }
   }
